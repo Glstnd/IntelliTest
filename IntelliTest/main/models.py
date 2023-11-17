87724ce305f50eccf4c09a09f72d2a62bb5f6f81
G.l.s.t.n.d <102985458+Glstnd@users.noreply.github.com>
2023-11-17 22:23:12 +0300

    change models for categories and questions

--- IntelliTest/main/models.py
+++ IntelliTest/main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class QuizCategory(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
+
+class QuizQuestion(models.Model):
+    category = models.ForeignKey(QuizCategory,
+                                 on_delete=models.CASCADE)
+    question = models.CharField(max_length=200)
+    opt_1 = models.CharField(max_length=200)
+    opt_2 = models.CharField(max_length=200)
+    opt_3 = models.CharField(max_length=200)
+    opt_4 = models.CharField(max_length=200)
+    level = models.CharField(max_length=100)
+    time_limit = models.IntegerField()
+    right_opt = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'Questions'
+
+    def __str__(self):
+        return self.question
