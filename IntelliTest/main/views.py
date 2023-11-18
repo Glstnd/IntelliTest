12875dbb3b968efb28b3c51eb0a689dd6716c85f
G.l.s.t.n.d <102985458+Glstnd@users.noreply.github.com>
2023-11-18 19:41:22 +0300

    bad commit

--- IntelliTest/main/views.py
+++ IntelliTest/main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from . import forms
 from .models import QuizCategory, QuizQuestion
 
@@ -23,7 +25,20 @@ def all_categories(request):
     return render(request, 'all-category.html', {'data': cat_data})
 
 
+@login_required
 def category_questions(request, cat_id):
     category = QuizCategory.objects.get(id=cat_id)
-    questions = QuizQuestion.objects.filter(category=category)
-    return render(request, 'category-questions.html', {"questions": questions, "category": category})
+    question = QuizQuestion.objects.filter(category=category).order_by('id').first()
+    return render(request, 'category-questions.html', {"questions": question, "category": category})
+
+
+@login_required
+def submit_answer(request, cat_id, quest_id=1):
+    if request.method == 'POST':
+        if 'skip' in request.POST:
+            return HttpResponse('Skip is clicked!')
+        category = QuizCategory.objects.get(id=cat_id)
+        question = QuizQuestion.objects.filter(category=category).order_by('id').first()
+        return render(request, 'category-questions.html', {"questions": question, "category": category})
+    else:
+        return HttpResponse('Method not allowed!!!')
