577915cf418bcfab120fc3cb38992ef2024053fd
G.l.s.t.n.d <102985458+Glstnd@users.noreply.github.com>
2023-11-20 03:26:36 +0300

    change test page and add click skip

--- IntelliTest/main/views.py
+++ IntelliTest/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from . import forms
 from .models import QuizCategory, QuizQuestion
 from django.contrib.auth.decorators import login_required
@@ -29,3 +30,15 @@ def category_questions(request, cat_id):
     category = QuizCategory.objects.get(id=cat_id)
     question = QuizQuestion.objects.filter(category=category).order_by('id').first()
     return render(request, 'category-questions.html', {'question': question, 'category': category})
+
+
+@login_required
+def submit_answer(request, cat_id, quest_id):
+    if request.method == 'POST':
+        if 'skip' in request.POST:
+            return HttpResponse('Skip is clicked!!!')
+        category = QuizCategory.objects.get(id=cat_id)
+        question = QuizQuestion.objects.filter(category=category).order_by('id').first()
+        return render(request, 'category-questions.html', {'question': question, 'category': category})
+    else:
+        return HttpResponse('Method not allowed!!!')
